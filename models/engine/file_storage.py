10fdc9f669a949e6e793819c837dd22526bdb0f6
khaledolemy <khaledolemy@gmail.com>
2024-08-12 15:13:07 +0300

    Update engine's capabilities | Add delete/get/count

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """File Storage Class to handle storage"""
 import json
+import models
 from models.base_model import BaseModel
 from models.user import User
 from models.message import Message
@@ -14,8 +15,15 @@ class FileStorage():
     __file_path = "TechieMateFS.json"
     __objects = {}
 
-    def all(self):
-        return self.__objects
+    def all(self, cls=None):
+        if cls:
+            out_dict = {}
+            for key, val in FileStorage.__objects.items():
+                if f"{cls}." in key:
+                    out_dict[key] = val
+            return out_dict
+        else:
+            return self.__objects
 
     def new(self, obj):
         self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
@@ -37,3 +45,33 @@ class FileStorage():
                     self.new(eval(class_name)(**val))
         except FileNotFoundError:
             return
+
+    def delete(self, obj=None):
+        if obj:
+            search_key = f"{obj.to_dict()['__class__']}.{obj.to_dict()['id']}"
+            if search_key in FileStorage.__objects:
+                del (FileStorage.__objects[search_key])
+                self.save
+
+    def close(self):
+        """
+        calls reload for deserializing the JSON file to objects
+        """
+        self.reload()
+
+    def get(self, cls, id):
+        if not cls or not id:
+            return None
+        instances = self.all(cls)
+        desired_instance = None
+        for _, instance in instances.items():
+            if instance.id == id:
+                desired_instance = instance
+                break
+        return desired_instance
+
+    def count(self, cls=None):
+        if cls:
+            return len(models.storage.all(cls))
+        else:
+            return len(models.storage.all())
